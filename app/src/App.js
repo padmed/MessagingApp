859d9111179e108b15e104286b4bae00ba5e5472
padmed <ddarchinian@gmail.com>
2023-12-02 19:05:47 +0400

    Work on general app features

--- app/src/App.js
+++ app/src/App.js
@@ -1,105 +1,25 @@
 import RegisterUser from "./views/RegisterUser";
 import LoginUser from "./views/LoginUser";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import LogoutButton from "./components/LogoutButton";
-// // initialize gun locally
-// const gun = Gun({
-//   peers: ["http://localhost:3030/gun"],
-// });
-
-// // create the initial state to hold the messages
-// const initialState = {
-//   messages: [],
-// };
-
-// // Create a reducer that will update the messages array
-// function reducer(state, message) {
-//   // Check if the message is already in the state
-//   const messageExists = state.messages.some(
-//     (existingMessage) => existingMessage.createdAt === message.createdAt
-//   );
-
-//   // If the message is not in the state, add it; otherwise, return the current state
-//   return messageExists
-//     ? state
-//     : {
-//         messages: [message, ...state.messages],
-//       };
-// }
-
-// export default function App() {
-//   // the form state manages the form input for creating a new message
-//   const [formState, setForm] = useState({
-//     name: "",
-//     message: "",
-//   });
-
-//   // initialize the reducer & state for holding the messages array
-//   const [state, dispatch] = useReducer(reducer, initialState);
-
-//   // when the app loads, fetch the current messages and load them into the state
-//   // this also subscribes to new data as it changes and updates the local state
-//   useEffect(() => {
-//     const messages = gun.get("messages");
-
-//     messages.map().once((m) => {
-//       dispatch({
-//         name: m.name,
-//         message: m.message,
-//         createdAt: m.createdAt,
-//       });
-//     });
-//   }, []);
-
-//   // set a new message in gun, update the local state to reset the form field
-//   function saveMessage() {
-//     gun.get("messages").set({
-//       name: formState.name,
-//       message: formState.message,
-//       createdAt: Date.now(),
-//     });
-
-//     // messages.set({});
-
-//     setForm({
-//       name: "",
-//       message: "",
-//     });
-//   }
-
-//   // update the form state as the user types
-//   function onChange(e) {
-//     setForm({ ...formState, [e.target.name]: e.target.value });
-//   }
-
-//   return (
-//     <div style={{ padding: 30 }}>
-//       <input
-//         onChange={onChange}
-//         placeholder="Name"
-//         name="name"
-//         value={formState.name}
-//       />
-//       <input
-//         onChange={onChange}
-//         placeholder="Message"
-//         name="message"
-//         value={formState.message}
-//       />
-//       <button onClick={saveMessage}>Send Message</button>
-//       {state.messages.map((message) => (
-//         <div key={message.createdAt}>
-//           <h2>{message.message}</h2>
-//           <h3>From: {message.name}</h3>
-//           <p>Date: {message.createdAt}</p>
-//         </div>
-//       ))}
-//     </div>
-//   );
-// }
+import ContactSearchBar from "./components/ContactSearchBar";
+import { initUsers } from "./reducers/users";
+import { authUser } from "./reducers/currentUser";
+import { useEffect } from "react";
 
 const App = () => {
   const currentUser = useSelector((state) => state.currentUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(initUsers());
+
+    const authedUserStr = window.localStorage.getItem("authedUser");
+    const authedUser = JSON.parse(authedUserStr);
+    if (authedUser) {
+      dispatch(authUser(authedUser.key));
+    }
+  }, []);
 
   return (
     <>
@@ -109,7 +29,10 @@ const App = () => {
           <LoginUser />
         </>
       ) : (
-        <LogoutButton />
+        <>
+          <LogoutButton />
+          <ContactSearchBar />
+        </>
       )}
     </>
   );
