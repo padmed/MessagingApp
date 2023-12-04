42f5dd44cc74dba1c68bf0b3eff49964ea679d45
padmed <ddarchinian@gmail.com>
2023-12-05 01:04:12 +0400

    Update function

--- app/src/utils/helpers.js
+++ app/src/utils/helpers.js
@@ -1,18 +1,19 @@
 import { gun } from "../models";
 
-export const saveInAllUsers = (key) => {
-  let alias;
-  gun
-    .user(key)
-    .get("alias")
-    .once((a) => {
-      alias = a;
+export const saveInAllUsers = async (alias, key) => {
+  await gun
+    .get("users")
+    .get(key)
+    .put({ alias, key }, (ack) => {
+      if (ack.err) {
+        console.error("Error while saving user in users database", ack.err);
+      } else {
+        console.log("User saved in users database");
+      }
     });
-
-  gun.get("users").set({ alias, key });
 };
 
-export const saveInLocalStrg = (username, key) => {
-  const authUserStr = JSON.stringify({ username, key });
+export const saveInLocalStrg = (alias, key) => {
+  const authUserStr = JSON.stringify({ alias, key });
   window.localStorage.setItem("authedUser", authUserStr);
 };
