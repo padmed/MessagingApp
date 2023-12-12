d4323808891e178425bbfc7d71378c0c40d3e5af
padmed <ddarchinian@gmail.com>
2023-12-12 18:30:43 +0400

    Add helper func

--- app/src/utils/helpers.js
+++ app/src/utils/helpers.js
@@ -2,3 +2,8 @@ export const saveInLocalStrg = (userObj) => {
   const authUserStr = JSON.stringify(userObj);
   window.localStorage.setItem("authedUser", authUserStr);
 };
+
+export const getUserFromLocalStrg = () => {
+  const user = JSON.parse(window.localStorage.getItem("authedUser"));
+  return user;
+};
